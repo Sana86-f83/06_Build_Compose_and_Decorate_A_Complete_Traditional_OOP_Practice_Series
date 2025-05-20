ea11a90f43866d5e9b8272403971e82280bfb448
Sana86-f83 <sana.faisalmehmood@gmail.com>
2025-05-20 13:34:55 +0500

    update

---
+++ 12_static_method.py
@@ -0,0 +1,13 @@
+# 2. Static Methods
+# Assignment:
+# Create a class TemperatureConverter with a static method celsius_to_fahrenheit(c) that returns the Fahrenheit value.
+
+class TemperatureConverter:
+
+    @staticmethod
+    def celsius_to_fahrenheit(c):
+        return (c * 9/5) + 32
+    
+
+if __name__ == "__main__":
+    print("Fahrenheit Value :",TemperatureConverter.celsius_to_fahrenheit(34))    
